9004e78fc9ecb4c6b8b29c473f8220e22cb51e86
alexisfrost1 <70116175+alexisfrost1@users.noreply.github.com>
2020-10-20 17:32:44 -0300

    AAñadiendo modulos, imports y providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiService } from './providers/api.service';
+import { AuthService } from './providers/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Toast } from '@ionic-native/toast/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(),HttpClientModule,FormsModule,ReactiveFormsModule, AppRoutingModule],
   providers: [
     StatusBar,
+    ApiService,
+    AuthService,
     SplashScreen,
+    Toast,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
