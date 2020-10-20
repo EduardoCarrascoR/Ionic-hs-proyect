78d612883ec9ead8ef9a5ae195a858faabe1640f
alexisfrost1 <70116175+alexisfrost1@users.noreply.github.com>
2020-10-20 17:22:33 -0300

    Añadiendo models para el guard interdace

---
+++ src/models/guard.interface.ts
@@ -0,0 +1,14 @@
+export interface Guard {
+
+    id?: number;
+    firstname: string;
+    lastname: string;
+    email?: string;
+    rut: string;
+    phone: string;
+    createdAt?: any;
+    lastLogin?: any;
+    accessToken?: string;
+    emailVerified?: string;
+    password?: string;
+  }
