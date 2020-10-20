89254552a298dd6bc74abb9f542c46ed0f5b15fd
alexisfrost1 <70116175+alexisfrost1@users.noreply.github.com>
2020-10-20 17:24:21 -0300

    Añadiendo carpeta providers con api y auth service

---
+++ src/providers/api.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Guard } from '../models/guard.interface'
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  // private apiUrl = 'http://localhost:3000/api'
+  private apiUrl = 'https://highsecurity-app.herokuapp.com/api'
+
+  constructor(
+    private http: HttpClient
+  ) {
+
+  }
+
+  login(rut: string, password: string): Observable<Guard> {
+    return this.http.post<Guard>(this.apiUrl + '/auth/login', { rut, password });
+  }
+
+  getGuardById(): Observable<Guard[]> {
+    return this.http.get<Guard[]>(this.apiUrl + '/users');
+  }
+
+  //addIncident(): 
+/*  getAllActivities(course_id: string, unit_number: string, lesson_number: string): Observable<Activity[]> {
+    return this.http.get<Activity[]>(`${this.apiUrl}/course/${course_id}/unit/${unit_number}/lesson/${lesson_number}/activities`);
+  } */
+
+ /*  getTestByCourseId(course_id: string, unit_number: string, lesson_number: string): Observable<Test> {
+    return this.http.get<Test>(`${this.apiUrl}/course/${course_id}/unit/${unit_number}/lesson/${lesson_number}/test`);
+  } */
+
+
+
+}
