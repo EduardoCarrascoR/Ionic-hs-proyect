72d8f48abdb39c4fff67f4cc1e9d73109455ab02
alexisfrost1 <70116175+alexisfrost1@users.noreply.github.com>
2020-10-21 18:34:58 -0300

    Subiendo provider para el auth  y los service

---
+++ src/app/providers/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Guard } from '../models/guard.interface';
+import { Toast } from '@ionic-native/toast/ngx';
+import {Router} from '@angular/router';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+        
+  constructor(
+    private api: ApiService,
+    private router: Router,
+    private toast: Toast
+   ) { 
+
+  }
+  login (userdata: Guard){
+    this.api.login(userdata.rut,userdata.password).toPromise()
+      .then((res:any)=>{
+        if (res.success) {
+          localStorage.setItem('guard',res.data)
+          this.router.navigate(['/tabs/tab1'])
+        }else{
+          throw new Error('no se logro logear')
+        }
+      })
+    .catch(error=>{
+        this.toast.show(error.error.mesage, '3000', 'center').toPromise()
+       .then(toast=> {
+         console.log(toast)
+       })
+      })
+  }
+}
