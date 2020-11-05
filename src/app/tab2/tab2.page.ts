6fdcefbe012045db52bd8412ced6b573b677e426
alexisfrost1 <70116175+alexisfrost1@users.noreply.github.com>
2020-11-05 15:34:14 -0300

    Arreglando vista e inscripción de visitas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -14,42 +14,35 @@ export class Tab2Page {
 
   private visitas = [{
     id: 1,
-    title: 'Eduardo Carrasco',
+    title: 'Eduardo Carra',
     Rut: '8.888.888-2',
+    Patente: 'GKSB78',
   },
   {
     id: 2,
     title: 'Eduardo Muñoz',
     Rut: '7.777.777-2',
+    Patente: 'GKSB78',
   },
   {
     id: 3,
     title: 'Eduardo Cabrera',
     Rut: '5.555.555-2',
+    Patente: 'GKSB78',
 
   },
   {
     id: 4,
     title: 'Eduardo Aguilar',
     Rut: '6.676.676-2',
+    Patente: 'GKSB78',
 
   },
   {
     id: 5,
     title: 'Eduardo Muñoz',
     Rut: '7.777.777-2',
-  },
-  {
-    id: 6,
-    title: 'Eduardo Cabrera',
-    Rut: '5.555.555-2',
-
-  },
-  {
-    id: 7,
-    title: 'Eduardo Aguilar',
-    Rut: '6.676.676-2',
-
+    Patente: 'GKSB78',
   }]
   
   constructor(
